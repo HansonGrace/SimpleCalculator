3fc7145348d2e098e727176b43d3813d72faf6b5
Grace Hanson <hansongrace2004@gmail.com>
2024-12-28 00:27:59 -0500

    start interface with basic calculator function buttons

---
+++ src/CalcGUI.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.security.Key;
+import java.text.DecimalFormat;
+
+
+public class CalcGUI {
+	
+	 Prakseis praksh = new Prakseis();
+	 JButton buttonAc = new JButton("AC");
+	 JButton buttonPlusMinus = new JButton("+/-");
+	 JButton buttonPercentage = new JButton("%");
+	 JButton buttonDia = new JButton("÷");
+	 JButton buttonEpi = new JButton("X");
+	 JButton buttonMinus = new JButton("-");
+	 JButton buttonPlus = new JButton("+");
+	 JButton buttonEquals = new JButton("=");
+	 JButton buttonKomma = new JButton(",");
+
+}
