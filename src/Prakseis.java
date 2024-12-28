cf21d804ae2790dcd603eb29d34dfc82e2a06fb5
Grace Hanson <hansongrace2004@gmail.com>
2024-12-28 16:12:18 -0500

    Add prakseis to set up rest of CalcGUI interface functionality

--- src/Prakseis.java
+++ src/Prakseis.java
@@ -1,4 +1,36 @@
-
 public class Prakseis {
+
+    double Plus(double a,double b){
+        return a+b;
+    }
+
+    double Minus(double a,double b){
+        return  a-b;
+    }
+
+    double Div(double a,double b){
+        if(a!=0 && b!=0){
+            return a/b;
+        }else{
+            return -1;
+        }
+    }
+
+    double Multiply(double a,double b){
+        return a*b;
+    }
+
+    double PlusMinus(double a ){
+        return -1*a;
+    }
+
+    double Percentage(double a){
+        return a/100;
+    }
+
+
+
+
+
 
 }
